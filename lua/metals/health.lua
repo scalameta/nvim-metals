ab3375bc7bf25a6aec0ddea91b7ced998146c51c
ckipp01 <ckipp@pm.me>
2021-01-16 16:22:00 +0100

    Get rid of os_capture command.
    I didn't realize that vim.fn.system will do literally
    the same thing. I should just use this.

--- lua/metals/health.lua
+++ lua/metals/health.lua
@@ -1,7 +1,6 @@
 local uv = vim.loop
 local setup = require 'metals.setup'
 local messages = require 'metals.messages'
-local util = require 'metals.util'
 
 local health_start = vim.fn['health#report_start']
 local health_ok = vim.fn['health#report_ok']
@@ -11,7 +10,7 @@ local health_error = vim.fn['health#report_error']
 local M = {}
 
 M.checkHealth = function()
-  health_start('Checkign env for nvim-metals')
+  health_start('Checking env for nvim-metals')
 
   local os = uv.os_uname().sysname
 
@@ -30,7 +29,7 @@ M.checkHealth = function()
   local metals_installed = uv.fs_stat(setup.metals_bin)
 
   if metals_installed then
-    local info = util.os_capture(setup.metals_bin .. ' --version', true)
+    local info = vim.fn.system(setup.metals_bin .. ' --version')
     health_ok('Metals found')
     health_info(info)
   else
