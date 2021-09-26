efe0da61f7700c7bb2bd1c0ce9ce968e17eb1a4c
Chris Kipp <ckipp@pm.me>
2021-09-26 14:29:46 +0200

    Refactor out handle decoder response.
    With upcoming changes of the TASTy stuff being merged in
    to the file decoder stuff, we can simplify the logic since
    it shares a common return value

--- lua/metals/decoder.lua
+++ lua/metals/decoder.lua
@@ -9,48 +9,52 @@ local function filename_from_uri(full_uri)
   return parts[#parts]
 end
 
+local handle_decoder_response = function(result, uri, decoder, format)
+  if result.error then
+    log.error_and_show(result.error)
+  else
+    local filename = filename_from_uri(uri)
+    local name = string.format("%s %s %s viewer", filename, format or "", decoder)
+    local cwd = fn.getcwd()
+
+    local exists = nil
+
+    local bufs = api.nvim_list_bufs()
+
+    for _, buf in pairs(bufs) do
+      local bufname = api.nvim_buf_get_name(buf)
+      local joined = util.path.join(cwd, name)
+      if bufname == joined then
+        exists = buf
+        break
+      end
+    end
+
+    local returned_value = result.value
+    local lines = util.split_on(returned_value, "\n")
+
+    if exists then
+      api.nvim_buf_set_lines(exists, 0, -1, false, lines)
+      api.nvim_win_set_buf(0, exists)
+    else
+      local new_buffer = api.nvim_create_buf(true, true)
+      api.nvim_buf_set_lines(new_buffer, 0, -1, false, lines)
+      api.nvim_buf_set_name(new_buffer, name)
+      -- TODO do we want to set this for everything?
+      -- TODO if this is a javap decoder, maybe we should set it to java
+      api.nvim_buf_set_option(new_buffer, "syntax", "scala")
+      api.nvim_win_set_buf(0, new_buffer)
+    end
+  end
+end
+
 local make_handler = function(uri, decoder, format)
   return function(err, _, result)
     if err then
       log.error_and_show(string.format("Something went wrong trying to get %s. Please check the logs.", decoder))
       log.error(err)
     else
-      if result.error then
-        log.error_and_show(result.error)
-      else
-        local filename = filename_from_uri(uri)
-        local name = string.format("%s %s %s viewer", filename, format or "", decoder)
-        local cwd = fn.getcwd()
-
-        local exists = nil
-
-        local bufs = api.nvim_list_bufs()
-
-        for _, buf in pairs(bufs) do
-          local bufname = api.nvim_buf_get_name(buf)
-          local joined = util.path.join(cwd, name)
-          if bufname == joined then
-            exists = buf
-            break
-          end
-        end
-
-        local returned_value = result.value
-        local lines = util.split_on(returned_value, "\n")
-
-        if exists then
-          api.nvim_buf_set_lines(exists, 0, -1, false, lines)
-          api.nvim_win_set_buf(0, exists)
-        else
-          local new_buffer = api.nvim_create_buf(true, true)
-          api.nvim_buf_set_lines(new_buffer, 0, -1, false, lines)
-          api.nvim_buf_set_name(new_buffer, name)
-          -- TODO do we want to set this for everything?
-          -- TODO if this is a javap decoder, maybe we should set it to java
-          api.nvim_buf_set_option(new_buffer, "syntax", "scala")
-          api.nvim_win_set_buf(0, new_buffer)
-        end
-      end
+      handle_decoder_response(result, uri, decoder, format)
     end
   end
 end
@@ -58,6 +62,7 @@ end
 return {
   command = "metals.file-decode",
   javap = "javap",
+  handle_decoder_response = handle_decoder_response,
   make_handler = make_handler,
   metals_decode = "metalsDecode:",
   semanticdb = "semanticdb",
