e7fed4169eeb5d6f2215633e2165dbcd097d0056
Cesar Enrique Ramirez <caenrique93@gmail.com>
2022-10-17 13:39:21 +0200

    fix: scape % before setting metals_status

--- lua/metals/status.lua
+++ lua/metals/status.lua
@@ -2,7 +2,9 @@ local log = require("metals.log")
 
 -- @param status (string) a new status to be displayed
 local function set_status(status)
-  vim.api.nvim_set_var("metals_status", status)
+  -- Scaping the status to prevent % characters breaking the statusline
+  local scaped_status = status:gsub('[%%]', '%%%1')
+  vim.api.nvim_set_var("metals_status", scaped_status)
 end
 
 local function prompt_command(status)
