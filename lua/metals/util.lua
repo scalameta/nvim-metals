335c1988952003ce4bf78436a982c6b7f21f80bb
Chris Kipp <ckipp@pm.me>
2023-11-24 16:47:30 +0100

    chore: small cleanup of unused stuff and add some types

--- lua/metals/util.lua
+++ lua/metals/util.lua
@@ -15,11 +15,11 @@ M.has_bins = function(...)
   return true
 end
 
--- Checks to see if a user given table is defined. If so, merge it with a default.
--- If not, just return the default table. This function favors the userTable on merge.
--- @param defaultTable The default table to return or merge
--- @param userTable The user defined table to check if exists and then merge
--- @return a new table that is either the default or merged with the user one.
+--- Checks to see if a user given table is defined. If so, merge it with a default.
+--- If not, just return the default table. This function favors the userTable on merge.
+--- @param defaultTable table The default table to return or merge
+--- @param userTable table The user defined table to check if exists and then merge
+--- @return table that is either the default or merged with the user one.
 M.check_exists_and_merge = function(defaultTable, userTable)
   -- TODO should we add another check in here to ensure that a key that a user
   -- is trying to set actually exists in the default table?
@@ -30,21 +30,14 @@ M.check_exists_and_merge = function(defaultTable, userTable)
   end
 end
 
--- Location of any files or executables that nvim-metals will create on your system
+--- Location of any files or executables that nvim-metals will create on your system
+---@type string
 M.nvim_metals_cache_dir = Path.new(vim.fn.stdpath("cache"), "nvim-metals")
 
---- Strip the leading and trailing spaces of a string
---- @param s string the string you want to trim.
-M.full_trim = function(s)
-  return (s:gsub("^%s*(.-)%s*$", "%1"))
-end
-
---- Strip trailing whites and trailing empty lines
---- @param s string the string you want to trim.
-M.trim_end = function(s)
-  return string.gsub(s, "[ \t]+%f[\r\n%z]", "")
-end
-
+--- Given a delimter split a string into a table.
+---@param s string
+---@param delimiter string
+---@return table of split results
 M.split_on = function(s, delimiter)
   local result = {}
   local from = 1
@@ -105,6 +98,7 @@ end
 
 M.is_windows = vim.loop.os_uname().version:match("Windows")
 
+---@return integer|nil
 M.find_metals_buffer = function()
   local metals_buf = nil
   local bufs = api.nvim_list_bufs()
