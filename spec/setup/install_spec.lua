b9d51f0d77ac8b4fa77778ccc2299cdb783ff679
Chris Kipp <ckipp@pm.me>
2025-12-20 11:32:00 +0100

    chore: update to a valid snapshot

--- spec/setup/install_spec.lua
+++ spec/setup/install_spec.lua
@@ -34,7 +34,7 @@ describe("install", function()
 
   it("should be able to install with a new 2.13 snapshot", function()
     local bare_config = require("metals.setup").bare_config()
-    bare_config.settings = { serverVersion = "1.6.1+6-b1f4d21d-SNAPSHOT" }
+    bare_config.settings = { serverVersion = "1.6.4+48-e2f699ed-SNAPSHOT" }
     config.validate_config(bare_config, vim.api.nvim_get_current_buf())
 
     eq(path.exists(metals_path), false)
